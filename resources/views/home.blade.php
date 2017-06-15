3cc4457bbd99d5948b616bef0e60e5bed05fd497
Pavel Gontarenko <pavgont@gmail.com>
2017-06-15 12:47:10 +0300

    Вывод пользователей view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,27 @@
 
                 <div class="panel-body">
                     You are logged in!
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>IP</th>
+                            <th>Last activity</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <th>{{$user->id}}</th>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->getLastIP()}}</td>
+                                <td>{{$user->getLastActivity()}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
