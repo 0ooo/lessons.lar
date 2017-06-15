7e88b93dc895848cc3f1fca87e74e1a176cac4f0
Pavel Gontarenko <pavgont@gmail.com>
2017-06-15 07:57:32 +0300

    Модель пользователя

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Models\History;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -28,10 +30,80 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * Последний ip пользователя.
+     *
+     * @var string
+     */
+    protected $lastIP = null;
+
+    /**
+     * Последняя активность пользователя.
+     *
+     * @var Carbon
+     */
+    protected $lastActivity = null;
+
+    /**
+     * Получение ключа для записи в cache.
+     *
+     * @return string
+     */
     public function getCacheKey () {
         if (! $this->exists) {
             throw new ModelNotFoundException();
         }
+
         return $this->getTable() . '#' . $this->id;
+    }
+
+    /**
+     * Получение данных о активности пользователя.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function histories()
+    {
+        return $this->hasMany(History::class);
+    }
+
+    /**
+     * Получение последнего IP.
+     *
+     * @return string
+     */
+    public function getLastIP()
+    {
+        return $this->lastIP;
+    }
+
+    /**
+     * Установить последний IP.
+     *
+     * @param string $lastIP
+     */
+    public function setLastIP($lastIP)
+    {
+        $this->lastIP = $lastIP;
+    }
+
+    /**
+     * Получение времени последней активности.
+     *
+     * @return string
+     */
+    public function getLastActivity()
+    {
+        return $this->lastActivity ? $this->lastActivity->diffForHumans() : null;
+    }
+
+    /**
+     * Установка времени последней активности.
+     *
+     * @param Carbon $lastActivity
+     */
+    public function setLastActivity($lastActivity)
+    {
+        $this->lastActivity = $lastActivity;
     }
 }
