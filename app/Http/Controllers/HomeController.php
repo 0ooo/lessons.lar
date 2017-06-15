27f2bf956191806aac6cdbcd56c5a5577c933818
Pavel Gontarenko <pavgont@gmail.com>
2017-06-15 08:16:28 +0300

    Выод всех пользователей

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -17,12 +19,26 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Получаем список пользователей и последнюю активность.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $users = User::all();
+
+        // Для каждого пользователя получаем ключ
+        $users->each(function (User $user) {
+
+            if (!cache()->has($user->getCacheKey())) {
+                return;
+            }
+
+            $data = cache($user->getCacheKey());
+            $user->setLastIP($data['ip']);
+            $user->setLastActivity(new Carbon($data['time']));
+        });
+
+        return view('home', compact('users'));
     }
 }
