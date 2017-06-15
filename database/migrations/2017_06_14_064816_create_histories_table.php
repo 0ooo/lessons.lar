c2302161306a80d2835d8670e8374deb165df4e1
Pavel Gontarenko <pavgont@gmail.com>
2017-06-15 08:14:18 +0300

    Миграция для History

---
+++ database/migrations/2017_06_14_064816_create_histories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('ip');
+            $table->enum('state', ['logged in', 'logged out']);
+            $table->timestamp('logged_at')->nullable();
+            $table->timestamp('logged_out_at')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
